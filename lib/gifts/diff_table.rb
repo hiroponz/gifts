3a11b753642f50f32aa4171502468e018af558dc
Sato Hiroyuki <sathiroyuki@gmail.com>
2013-03-31 07:13:24 +0000

    DiffTable#add returns added records.

--- lib/gifts/diff_table.rb
+++ lib/gifts/diff_table.rb
@@ -19,21 +19,28 @@ module Gifts
         return table.select { |record| record.commit == db_commit }
       end
 
-      git_commit.diffs.each do |git_diff|
-        next if git_diff.diff.empty?
+      result = []
 
-        db_file = @db.files.add(git_commit, git_diff, db_commit)
+      begin
+        git_commit.diffs.each do |git_diff|
+          next if git_diff.diff.empty?
 
-        if db_file && db_file.type == FileTable::TypeText
-          key = db_commit.id.to_s + ":" + db_file.id.to_s
-          table[key] || table.add(key, commit: db_commit.key, file: db_file.key, diff: diff_content(git_diff.diff))
+          db_file = @db.files.add(git_commit, git_diff, db_commit)
+
+          if db_file && db_file.type == FileTable::TypeText
+            key = db_commit.id.to_s + ":" + db_file.id.to_s
+            db_diff = table[key] || table.add(key, commit: db_commit.key, file: db_file.key, diff: diff_content(git_diff.diff))
+            result << db_diff
+          end
         end
+      rescue Grit::Git::GitTimeout => e
+        puts "Timeout commit:#{git_commit.id}"
+        db_commit.status = CommitTable::StatusTimeout
+      else
+        db_commit.status = CommitTable::StatusCompleted
       end
-    rescue Grit::Git::GitTimeout => e
-      puts "Timeout commit:#{git_commit.id}"
-      db_commit.status = CommitTable::StatusTimeout
-    else
-      db_commit.status = CommitTable::StatusCompleted
+
+      result
     end
 
     def diff_content(diff)
