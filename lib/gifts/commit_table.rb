f339e2e015fb58ed9b45c8fad66b3dbb3f4571d2
Sato Hiroyuki <sathiroyuki@gmail.com>
2013-04-02 21:45:33 +0900

    Cache last commit key of repo.

--- lib/gifts/commit_table.rb
+++ lib/gifts/commit_table.rb
@@ -28,32 +28,41 @@ module Gifts
 
       offset = 0
       count = 100
+      processed_prev_commit = false
+
       begin
         commits = Grit::Commit.find_all(git_repo, nil, { all: true, date_order: true, max_count: count, skip: offset })
         commits.each do |git_commit|
           key = db_repo.id.to_s + ":" + git_commit.id
-          author = @db.users.add(git_commit.author.name)
-          committer = @db.users.add(git_commit.committer.name)
 
-          db_commit =
-            table[key] ||
-            table.add(
-              key,
-              author: author,
-              authored_date: git_commit.authored_date,
-              committer: committer,
-              committed_date: git_commit.committed_date,
-              message: git_commit.message,
-              repo: db_repo,
-              rev: git_commit.id,
-              status: StatusProcessing
-            )
+          if key == db_repo.last_commit_key
+            processed_prev_commit = true
+            break
 
-          db_diffs = @db.diffs.add(git_commit, db_commit)
-          result << db_commit
+          else
+            author = @db.users.add(git_commit.author.name)
+            committer = @db.users.add(git_commit.committer.name)
+
+            db_commit =
+              table[key] ||
+              table.add(
+                key,
+                author: author,
+                authored_date: git_commit.authored_date,
+                committer: committer,
+                committed_date: git_commit.committed_date,
+                message: git_commit.message,
+                repo: db_repo,
+                rev: git_commit.id,
+                status: StatusProcessing
+              )
+
+              db_diffs = @db.diffs.add(git_commit, db_commit)
+              result << db_commit
+          end
         end
         offset += count
-      end until commits.count == 0
+      end until commits.count == 0 || processed_prev_commit
 
       result
     end
