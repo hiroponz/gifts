f9dc5120c0063c67b50b3c640c4020d05efd31ea
Sato Hiroyuki <sathiroyuki@gmail.com>
2013-04-02 15:10:53 +0900

    Processing per 100 commits order by committed date

--- lib/gifts/commit_table.rb
+++ lib/gifts/commit_table.rb
@@ -26,29 +26,34 @@ module Gifts
     def add(git_repo, db_repo)
       result = []
 
-      Grit::Commit.find_all(git_repo, nil, { all: true }).each do |git_commit|
-        key = db_repo.id.to_s + ":" + git_commit.id
-        author = @db.users.add(git_commit.author.name)
-        committer = @db.users.add(git_commit.committer.name)
+      offset = 0
+      count = 100
+      begin
+        commits = Grit::Commit.find_all(git_repo, nil, { all: true, date_order: true, max_count: count, skip: offset })
+        commits.each do |git_commit|
+          key = db_repo.id.to_s + ":" + git_commit.id
+          author = @db.users.add(git_commit.author.name)
+          committer = @db.users.add(git_commit.committer.name)
 
-        db_commit =
-          table[key] ||
-          table.add(
-            key,
-            author: author.key,
-            authored_date: git_commit.authored_date,
-            committer: committer.key,
-            committed_date: git_commit.committed_date,
-            message: git_commit.message,
-            repo: db_repo.key,
-            rev: git_commit.id,
-            status: StatusProcessing
-          )
+          db_commit =
+            table[key] ||
+            table.add(
+              key,
+              author: author.key,
+              authored_date: git_commit.authored_date,
+              committer: committer.key,
+              committed_date: git_commit.committed_date,
+              message: git_commit.message,
+              repo: db_repo.key,
+              rev: git_commit.id,
+              status: StatusProcessing
+            )
 
-        db_diffs = @db.diffs.add(git_commit, db_commit)
-
-        result << db_commit
-      end
+          db_diffs = @db.diffs.add(git_commit, db_commit)
+          result << db_commit
+        end
+        offset += count
+      end until commits.count == 0
 
       result
     end
