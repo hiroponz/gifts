7002779137b9c421f8ffbee5aba9a55a88b93a44
Sato Hiroyuki <sathiroyuki@gmail.com>
2013-04-02 13:59:06 +0900

    Fix return value of DiffTable#add

--- lib/gifts/diff_table.rb
+++ lib/gifts/diff_table.rb
@@ -15,32 +15,33 @@ module Gifts
     end
 
     def add(git_commit, db_commit)
-      if db_commit.status != CommitTable::StatusProcessing
-        return table.select { |record| record.commit == db_commit }
-      end
+      if db_commit.status == CommitTable::StatusProcessing
+        result = []
 
-      result = []
+        begin
+          git_commit.diffs.each do |git_diff|
+            next if git_diff.diff.empty?
 
-      begin
-        git_commit.diffs.each do |git_diff|
-          next if git_diff.diff.empty?
+            db_file = @db.files.add(git_commit, git_diff, db_commit)
 
-          db_file = @db.files.add(git_commit, git_diff, db_commit)
-
-          if db_file && db_file.type == FileTable::TypeText
-            key = db_commit.id.to_s + ":" + db_file.id.to_s
-            db_diff = table[key] || table.add(key, commit: db_commit.key, file: db_file.key, diff: diff_content(git_diff.diff))
-            result << db_diff
+            if db_file && db_file.type == FileTable::TypeText
+              key = db_commit.id.to_s + ":" + db_file.id.to_s
+              db_diff = table[key] || table.add(key, commit: db_commit.key, file: db_file.key, diff: diff_content(git_diff.diff))
+              result << db_diff
+            end
           end
+        rescue Grit::Git::GitTimeout => e
+          puts "Timeout commit:#{git_commit.id}"
+          db_commit.status = CommitTable::StatusTimeout
+        else
+          db_commit.status = CommitTable::StatusCompleted
         end
-      rescue Grit::Git::GitTimeout => e
-        puts "Timeout commit:#{git_commit.id}"
-        db_commit.status = CommitTable::StatusTimeout
-      else
-        db_commit.status = CommitTable::StatusCompleted
-      end
 
-      result
+        result
+      else
+        records = table.select { |record| record.commit == db_commit }
+        (records && records.collect) || []
+      end
     end
 
     def diff_content(diff)
