98009ca655eb71714c658bec03daaa88b590ebce
Sato Hiroyuki <sathiroyuki@gmail.com>
2013-04-05 21:43:59 +0900

    Separate diff into addition and deletion parts.

--- lib/gifts/term_table.rb
+++ lib/gifts/term_table.rb
@@ -14,7 +14,8 @@ module Gifts
         ) do |table|
           table.index("commit.message", with_position: true)
 
-          table.index("diff.diff", with_position: true)
+          table.index("diff.addition", with_position: true)
+          table.index("diff.deletion", with_position: true)
         end
       end
     end
